7b1885f6c10f7f8e69c7e54aa0e6f895652e59d7
Ben Schinn <schinn.ben@gmail.com>
2019-12-11 00:13:14 -0700

    2 color columns

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate image;
 
 use std;
-use image::{ImageBuffer, Rgb};
+use image::{GenericImage, GenericImageView, ImageBuffer, RgbImage};
 
 fn main() {
      let file = std::fs::File::open("./src/input.json")
@@ -12,10 +12,28 @@ fn main() {
              .expect("file should have color key");
      //println!("{:?}", colors)
 
-     // a default (black) image containing Rgb values
-     let mut image = ImageBuffer::<Rgb<u8>, Vec<u8> >::new(500, 500);
-     // set a central pixel to white
-     image.get_pixel_mut(10, 5);
-     // write it out to a file
-     image.save("output.png").unwrap();
+     // Construct a new RGB ImageBuffer with the specified width and height.
+     let img: RgbImage = ImageBuffer::new(512, 512);
+
+     // Construct a new by repeated calls to the supplied closure.
+     let mut img = ImageBuffer::from_fn(512, 512, |x, y| {
+         if x % 10 == 0 {
+             image::Rgb([179, 49, 46])
+         } else {
+             image::Rgb([23, 179, 78])
+         }
+     });
+
+     // Obtain the image's width and height.
+     let (width, height) = img.dimensions();
+
+     // Access the pixel at coordinate (2, 2).
+     let pixel = img[(100, 100)];
+
+     // Or use the ```get_pixel``` method from the ```GenericImage``` trait.
+     let pixel = *img.get_pixel(100, 100);
+
+     // Put a pixel at coordinate (2, 2).
+     img.put_pixel(100, 100, pixel);
+     img.save("output.png").unwrap();
 }
